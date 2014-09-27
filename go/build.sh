2d382739b83f71c215c5441e68fbb092069756dc
Keiji, Yoshimi <walf443@gmail.com>
2014-09-27 12:26:25 +0900

    gocacheとredisの初期設定

--- go/build.sh
+++ go/build.sh
@@ -5,4 +5,6 @@ go get github.com/go-sql-driver/mysql
 go get github.com/martini-contrib/render
 go get github.com/martini-contrib/sessions
 go get github.com/walf443/stopwatch
+go get github.com/fzzy/radix
+go get github.com/pmylund/go-cache
 go build -o golang-webapp .
