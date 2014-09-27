b9684e6a572e03101c452335fcd60ca3829a1b22
Keiji, Yoshimi <walf443@gmail.com>
2014-09-27 11:41:19 +0900

    update init.sh

--- go/init.sh
+++ go/init.sh
@@ -9,7 +9,7 @@ mydb=isu4_qualifier
 myhost=127.0.0.1
 myport=3306
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < ../sql/schema.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < ../sql/dummy_users.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < ../sql/dummy_log.sql
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < ../sql/alter.sql
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < /home/isucon/webapp/go/sql/schema.sql
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < /home/isucon/webapp/go/sql/dummy_users.sql
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < /home/isucon/webapp/go/sql/dummy_log.sql
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < /home/isucon/webapp/go/sql/alter.sql
---
+++ go/sql/alter.sql
@@ -0,0 +1,1 @@
+alter table login_log add index user_status (user_id, succeeded );
